87ddd44b8982f38261de3c6c9682755adfd36809
Komang Janardana <janardanakomang@gmail.com>
2022-06-21 07:59:53 +0700

    Add list from listArray to UiTableView

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    let listArray = ["Jack", "Queen", "King"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = listArray[indexPath.row]
+        return cell
+    }
+
+}
+
